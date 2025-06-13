fec50ca20d4395aedfa68d766f4144bffc25e149
Ali Rıza BAKIR <67209912+Alirizabakir@users.noreply.github.com>
2025-06-13 14:05:39 +0300

    Fix

--- project/src/router.js
+++ project/src/router.js
@@ -1,12 +1,5 @@
 const router = require('express').Router()
 
-
-router.use('/permission', require('./routes/permission'))
-router.use('/regions', require('./routes/regions'))
-router.use('/conditions', require('./routes/conditions'))
-router.use('/works', require('./routes/works'))
-router.use('/rooms', require('./routes/rooms'))
-router.use('/icons', require('./routes/icons'))
 router.use('/attributes', require('./routes/attributes'))
 
 module.exports = router
--- project/src/server.js
+++ project/src/server.js
@@ -5,6 +5,8 @@ const parser = require('body-parser')
 express.use(parser.json({ limit: '500kb' }))
 express.use(parser.urlencoded({ limit: '500kb', extended: true }))
 
+const redisAdaptor = require('./utils/redisAdaptor')
+
 express.use(async(req, res, next) => {
     console.log(req.originalUrl)
     if (req.headers.rediskey) {
@@ -19,10 +21,13 @@ express.use(async(req, res, next) => {
 
 express.use('/permission', require('./router'))
 
-express.listen(7015, () => {
+const {initialize, consumer} = require('./utils/kafka')
+initialize().then(() => { consumer() })
+
+express.listen(7014, () => {
     console.log('-------------------------------------------')
     console.log('Tiktak Service Permission')
-    console.log('PORT: 7015')
+    console.log('PORT: 7016')
     console.log(`Ortam: ${process.env.NODE_ENV}`)
     console.log('-------------------------------------------')
 })
